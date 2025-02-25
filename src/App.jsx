4dc40f13d5a2a9edc0020de09618352cba981470
parvgoel2706 <parvgoel2345@gmail.com>
2025-02-26 01:10:15 +0530

    adding initial files

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css";
+import SeacrhBox from "./Searchbox";
+
+function App() {
+  return <>
+  <SeacrhBox/>
+  </>;
+}
+
+export default App;
---
+++ src/Searchbox.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import SearchIcon from '@mui/icons-material/Search';
+
+
+export default function SeacrhBox() {
+  let [location, setLocation] = useState("");
+
+  let inputHandler = (event) => {
+    setLocation(event.target.value);
+  };
+
+  let submitHandler = (event) => {
+    event.preventDefault();
+    console.log(location);
+    setLocation("");
+  };
+
+  return (
+    <>
+      <h2>Enter the location:</h2>
+      <form onSubmit={submitHandler}>
+      <TextField id="searchBox" label="City" variant="outlined" name="SecrchBox"
+          onChange={inputHandler}
+          placeholder="Enter location"
+          value={location}
+        />
+        <br />
+        <br />
+        {/* <Button variant="contained" startIcon={<SearchIcon />}>Search</Button> */}
+      </form>
+    </>
+  );
+}
